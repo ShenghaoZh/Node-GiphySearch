779a018242b1f3329301395c1bbef23850aacde4
ShenghaoZh <shenghao@wustl.edu>
2020-11-12 12:15:13 -0800

    giphy search

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+const port  = process.env.PORT || 3000;
+
+const hbs  = require('express-handlebars');
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts/'
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  let queryString = req.query.term;
+  let term = encodeURIComponent(queryString);
+  let url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+  http.get(url, (response) => {
+    response.setEncoding('utf8');
+    let body = '';
+    response.on('data', (d) => {
+      body += d;
+    });
+    response.on('end', () => {
+      let parsed = JSON.parse(body);
+      res.render('search-giphy', { gifs: parsed.data })
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log('Giphy Search listening on port: ', port);
+});
